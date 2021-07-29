b374a69aa77673e18261e52bf437975f40d4f299
vjespinoza <vjespinoza84@gmail.com>
2021-07-29 16:56:46 -0500

    Product cards

--- src/components/store/productItem/productItem.js
+++ src/components/store/productItem/productItem.js
@@ -1,11 +1,14 @@
 import {
     ProductCard,
+    ProductInfo,
     ProductImage,
+    ImageWrapper,
     ProductName,
     ProductModel,
     ProductPrice,
-    ProductDescription,
+    TagWrapper,
     ProductSeller,
+    SizeWrapper,
     ProductSize,
     ProductTag,
 } from "./productItem.style";
@@ -13,31 +16,47 @@ import {
 const ProductItem = ({ shoe }) => {
     return (
         <ProductCard>
-            <ProductImage src={`./img/${shoe.image}`} />
-            {shoe.tags.map((tag, i) => {
-                return <ProductTag key={`tag-${i}`}>{tag}</ProductTag>;
-            })}
-            <ProductPrice>
-                <sup>$</sup>
-                {shoe.price}
-            </ProductPrice>
-            <ProductName>{shoe.name}</ProductName>
-            <ProductModel>{shoe.model}</ProductModel>
-            <ProductSeller>{shoe.seller}</ProductSeller>
-            <ProductDescription>
-                {shoe.description.map((item, i) => {
-                    return <li key={`item-${i}`}>{item}</li>;
-                })}
-            </ProductDescription>
-            <ProductSize>
-                {shoe.sizes.map((size, i) => {
-                    return (
-                        <option key={`opt-${i}`} value={size}>
-                            {size}
-                        </option>
-                    );
-                })}
-            </ProductSize>
+            <ImageWrapper>
+                <ProductImage src={`./img/${shoe.image}`} />
+            </ImageWrapper>
+            <ProductInfo>
+                <TagWrapper>
+                    {shoe.tags.map((tag, i) => {
+                        return <ProductTag key={`tag-${i}`}>{tag}</ProductTag>;
+                    })}
+                </TagWrapper>
+                <ProductPrice>
+                    <sup>$</sup>
+                    {shoe.price}
+                </ProductPrice>
+                <ProductName>{shoe.name}</ProductName>
+                <ProductModel>Modelo: {shoe.model}</ProductModel>
+                <ProductSeller>Vendedor: {shoe.seller}</ProductSeller>
+                <SizeWrapper>
+                    <label>Tallas:</label>
+                    <ProductSize>
+                        {shoe.sizes.map((size, i) => {
+                            return (
+                                <div key={`item-${shoe.id}-${i}`}>
+                                    <input
+                                        id={`input-${shoe.id}-${i}`}
+                                        key={`input-${shoe.id}-${i}`}
+                                        type="radio"
+                                        name={shoe.id}
+                                    ></input>
+                                    <label
+                                        htmlFor={`input-${shoe.id}-${i}`}
+                                        key={`label-${shoe.id}-${i}`}
+                                        value={size}
+                                    >
+                                        {size}
+                                    </label>
+                                </div>
+                            );
+                        })}
+                    </ProductSize>
+                </SizeWrapper>
+            </ProductInfo>
         </ProductCard>
     );
 };
--- src/components/store/productItem/productItem.style.js
+++ src/components/store/productItem/productItem.style.js
@@ -1,11 +1,122 @@
 import styled from "styled-components";
 
-export const ProductCard = styled.div``;
-export const ProductImage = styled.img``;
-export const ProductName = styled.h3``;
-export const ProductModel = styled.p``;
-export const ProductPrice = styled.p``;
-export const ProductDescription = styled.ul``;
-export const ProductSeller = styled.span``;
-export const ProductSize = styled.select``;
-export const ProductTag = styled.span``;
+export const ProductCard = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    max-width: 320px;
+    margin-bottom: 20px;
+    box-shadow: 2px 2px 4px 2px rgba(0, 0, 0, 0.2);
+
+    &:hover > div img {
+        transform: scale(1.1);
+        transition: all ease-in-out 200ms;
+    }
+`;
+
+export const ProductInfo = styled.div`
+    display: grid;
+    grid-template-areas:
+        "price price price price"
+        "tags tags tags tags"
+        "name name name name"
+        "model model model model"
+        "seller seller seller seller"
+        "size size size size";
+    grid-template-columns: repeat(4, 1fr);
+    padding: 20px;
+`;
+
+export const ImageWrapper = styled.div`
+    overflow: hidden;
+`;
+
+export const ProductImage = styled.img`
+    grid-area: image;
+    width: 100%;
+    transition: all ease-in-out 200ms;
+`;
+
+export const ProductName = styled.h3`
+    grid-area: name;
+    text-transform: uppercase;
+    margin: 20px 0 10px 0;
+    color: ${(props) => props.theme.colors.secondary};
+`;
+
+export const ProductModel = styled.p`
+    grid-area: model;
+    font-size: 0.8rem;
+    font-weight: bold;
+    margin-bottom: 5px;
+`;
+
+export const ProductPrice = styled.p`
+    grid-area: price;
+    font-size: 1.5rem;
+    text-align: right;
+    color: ${(props) => props.theme.colors.primary};
+    margin-bottom: 20px;
+`;
+
+export const ProductSeller = styled.span`
+    grid-area: seller;
+    font-size: 0.8rem;
+    margin-bottom: 5px;
+`;
+
+export const SizeWrapper = styled.form`
+    grid-area: size;
+    display: flex;
+    flex-direction: column;
+    padding-top: 20px;
+
+    & > label {
+        font-size: 1rem;
+        font-weight: bold;
+        margin-bottom: 10px;
+    }
+`;
+
+export const ProductSize = styled.fieldset`
+    display: flex;
+    flex-wrap: wrap;
+    gap: 15px;
+    height: 60px;
+    align-content: flex-start;
+    outline: none;
+    border: none;
+    color: ${(props) => props.theme.colors.font};
+
+    & input {
+        display: none;
+    }
+
+    & input:checked + label {
+        outline: 2px solid ${(props) => props.theme.colors.secondary};
+        color: ${(props) => props.theme.colors.secondary};
+        font-weight: bold;
+    }
+
+    & label {
+        display: block-size;
+        cursor: pointer;
+        outline: 1px solid ${(props) => props.theme.colors.font};
+        padding: 5px 8px;
+        width: 3rem;
+        text-align: center;
+        font-size: 0.8rem;
+    }
+`;
+
+export const TagWrapper = styled.div`
+    grid-area: tags;
+`;
+
+export const ProductTag = styled.span`
+    border: 1px solid ${(props) => props.theme.colors.font};
+    color: ${(props) => props.theme.colors.font};
+    font-size: 0.6rem;
+    padding: 3px 5px;
+    margin-right: 5px;
+`;
